5feac67b450fe77df197cfe4790a6c1dfea92a23
Tim Rodger <tim.rodger@gmail.com>
2014-11-30 14:30:35 +0000

    Updates Parser class comments

--- src/lib/parser.js
+++ src/lib/parser.js
@@ -1,11 +1,17 @@
 var Tokenizer = require('./tokenizer');
 
+/**
+ * Parser implements generating a set of commands from a script
+ * It validates the script syntax is correct
+ */
 function Parser(string) {
     this.tokenizer = new Tokenizer(string);
 };
 
 /**
  * Return a command object with a method and endpoint property
+ * Throw exception is script is invalid
+ * Should be 'operator endpoint' or just 'endpoint' which means GET 'endpoint'
  */
 Parser.prototype.next = function() {
     if (this.tokenizer.hasMore()){
