dd7b36f9cfcb6894d78eb70ef8c80396a3a290a5
Tim Rodger <tim.rodger@gmail.com>
2014-12-04 00:33:17 +0000

    Updates class comments

--- src/lib/token.js
+++ src/lib/token.js
@@ -1,8 +1,10 @@
 /**
  * Token class encapsulates a token with these public method
  *
+ * @todo add isUri method?
  * isOperator - test if the token is a script operator, eg. / or + (split and join)
  * isMethod - test if the token is a script method, eg. POST
+ * @todo use getValue for all types
  * getMethod - convert script operators into a request method
  * getValue - return the value of the token
  */
@@ -11,14 +13,25 @@ function Token(value) {
     this.value = value;
 }
 
+/**
+ * Operators are used by the glue script executor to operate on the current 'data'
+ * ie the body of the previous response
+ */
 Token.prototype.isOperator = function() {
     return (this.value[0] == '/');
 };
 
+/*
+ * A method tells the glue script executor how to make the next request
+ * extend to handle message queue interactions
+ */
 Token.prototype.isMethod = function() {
     return (this.value[0] == '>');
 };
 
+/**
+ * @obsolete - use getValue to retrieve all token strings
+ */
 Token.prototype.getMethod = function() {
     if ('>>' == this.value){
         return 'POST';
