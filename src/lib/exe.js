48c099e22e1d3f3a509f0156899b6dd218cc21c7
Tim Rodger <tim.rodger@gmail.com>
2014-12-04 22:38:46 +0000

    exe class updates to handle multiple requests

--- src/lib/exe.js
+++ src/lib/exe.js
@@ -12,6 +12,7 @@ var request = require('request');
 function Exe(parser, callback) {
     this.parser = parser;
     this.callback = callback;
+    this.body_count = 0;
 };
 
 /**
@@ -26,28 +27,56 @@ Exe.prototype.start = function() {
  * not all the previous response headers. Maybe use a composite type of string and mime-type
  */
 Exe.prototype.runNext = function(headers, body) {
-    var exe = this;
     var command = this.parser.next();
 
     if (command) {
-        console.log('runNext(): making request to : ' + command.uri);
+        if (command['split']){
+            // split body, expect json array
+            // call this.runNext(headers, bodies);
+        } else {
+            this.multiRequest(command, headers, body);
+        }
+    } else {
+        this.end(body);
+    }
+};
+
+Exe.prototype.multiRequest = function(command, headers, body) {
+    var exe = this;
+    // body might be a string or an array
+    var bodies = body;
+    if (!(body instanceof Array)){
+        var bodies = [body];
+    }
+
+    exe.body_count = bodies.length;
+    //console.log(bodies);
+    console.log('runNext(): making request to : ' + command.uri + ' ' + exe.body_count);
+
+    // generate a request per body item
+    for(var key in bodies) { 
         command['headers'] = headers;
-        // if command['split'] then split body assuming a json array and make a request per item
-        command['body'] = body;
-        // fire off async request
-        request(command, function(error, response, body) {
+        command['body'] = bodies[key];
+        request(command, function(error, response, response_body) {
             if (!error && response.statusCode == 200){
                 console.log('Success');
-                exe.runNext({'content-type' : response.headers['content-type']}, body);
+                // call receiveResponse 
+                exe.runNext({'content-type' : response.headers['content-type']}, response_body);
             } else {
                 // end the script here and respond
                 exe.end('Failure: ' + error);
             }
         });
-    } else {
-        this.end(body);
     }
 };
+
+/*
+ * Callback from each request
+*/
+Exe.prototype.receiveResponse = function(error, response, body) {
+    this.incoming_bodies.push({headers: headers, body: body});
+    // if incoming_bodies equals body_count then call runNext
+}
 
 Exe.prototype.end = function(result) {
     console.log('End');
