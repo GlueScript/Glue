5061674a63b65ae634f9a11f7002396579423aa4
Tim Rodger <tim.rodger@gmail.com>
2014-12-06 17:50:30 +0000

    More Payload implementation

--- src/lib/payload.js
+++ src/lib/payload.js
@@ -1,19 +1,20 @@
+var Parser = require('xmldom').DOMParser;
+
 /*
  * Contains a request payload
  * Has content and type
+ * Converts Object and Array content to JSON strings
 */
-
 function Payload(content, type) {
     if (content instanceof Object) {
         this.content = JSON.stringify(content);
         this.type = 'application/json';
     } else {
         this.content = content;
-        // now figure out type
-        if (isJSON(content)){
+        if (isJSON(content)) {
             this.type = 'application/json';
         } else {
-            this.type = 'text/plain';
+            this.type = getContentType(this.content);
         }
     }
 }
@@ -27,7 +28,21 @@ function isJSON(content) {
     }
 }
 
-function isXML(content) {
-
+function getContentType(content) {
+    try {
+        var doc = new Parser().parseFromString(content);
+        var documentElement = (doc ? doc.ownerDocument || doc : 0).documentElement;
+        if (documentElement) {
+            var name = documentElement.nodeName.toLowerCase();
+            if ('html' === name){
+                return 'text/html';
+            } else {
+                return 'application/xml';
+            }
+        }
+    } catch (e) {
+    }
+    return 'text/plain';
 }
+
 module.exports = Payload;
--- src/test/payload_test.js
+++ src/test/payload_test.js
@@ -1,7 +1,6 @@
 var Payload = require('../lib/payload'),
     assert = require('assert');
 
-
 describe('Payload', function() {
     describe('type', function() {
         it('should convert array to json', function(){
@@ -18,15 +17,36 @@ describe('Payload', function() {
             assert.equal(JSON.stringify(value), payload.content);
         });
 
-        it('should treat json string as json', function(){
+        it('should treat a json string as json', function(){
             var value = JSON.stringify({a : 'A', b : 'B', c: 'C'});
             var payload = new Payload(value);
             assert.equal('application/json', payload.type);
             assert.equal(value, payload.content);
         });
 
-        it('should treat unformatted string as text/plain', function(){
+        it('should treat an unformatted string as text/plain', function(){
             var value = 'A plain old string';
+            var payload = new Payload(value);
+            assert.equal('text/plain', payload.type);
+            assert.equal(value, payload.content);
+        });
+
+        it('should treat an xml string as application/xml', function(){
+            var value = '<?xml version="1.0" encoding="UTF-8" ?>' + "\n" + '<root>Value</root>';
+            var payload = new Payload(value);
+            assert.equal('application/xml', payload.type);
+            assert.equal(value, payload.content);
+        });
+
+        it('should treat a html string as text/html', function(){
+            var value = '<html>' + "\n" + '<html><head></head><body></body></html>';
+            var payload = new Payload(value);
+            assert.equal('text/html', payload.type);
+            assert.equal(value, payload.content);
+        });
+
+        it('should treat a broken xml string as text/plain', function(){
+            var value = '<?xml version="1.0" encoding="UTF-8" ?>' + "\n" + '<root Value</root>';
             var payload = new Payload(value);
             assert.equal('text/plain', payload.type);
             assert.equal(value, payload.content);
