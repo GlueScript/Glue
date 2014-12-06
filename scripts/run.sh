18d2c82cef0c7570c0127fa9662c5c0cb012ebd0
Tim Rodger <tim.rodger@gmail.com>
2014-12-06 10:32:10 +0000

    Adds script runner and fig config

---
+++ scripts/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# run a glue script
+# remember the content-type header!
+
+script=$1
+glue=http://192.168.59.103:41990/
+
+curl -X POST -d @$script -H 'Content-Type: text/plain' $glue
