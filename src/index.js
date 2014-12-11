5632263c5386810eb52a6cd13d41696a52b34b6f
Tim Rodger <tim.rodger@gmail.com>
2014-12-11 16:39:04 +0000

    Removes redundant whitespace

--- src/index.js
+++ src/index.js
@@ -21,5 +21,4 @@ var logger = new winston.Logger({
 // use env.PORT if set
 var PORT = process.env.PORT || 80;
 server.start(PORT);
-
 logger.log('info', 'Running glue service on http://localhost:' + PORT);
