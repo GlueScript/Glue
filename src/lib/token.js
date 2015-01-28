346ef23a7dde6f7bc73459cc4f7efbd4ba59b127
Tim Rodger <tim.rodger@gmail.com>
2015-01-28 21:59:01 +0000

    Removes redundant code

--- src/lib/token.js
+++ src/lib/token.js
@@ -1,12 +1,14 @@
 var _ = require('underscore');
 
 /**
- * Token class encapsulates a token with these public method
- * @todo add isNull method?
+ * Token class encapsulates a script token 
+ * @todo add isNull method? add a pipe | operator
+ *
  * isUri - test if the token is a uri
  * isOperator - test if the token is a script operator, eg. / or + (split and join)
  * isMethod - test if the token is a script method, eg. POST
  * value - return the value of the token
+ * type - return the type of the token
  */
 var operators = {
     '/' : 'split',
@@ -42,7 +44,6 @@ function Token(value) {
  */
 Token.prototype.isOperator = function() {
     return this.type === 'operator';
-    return _.has(operators, this._value);
 };
 
 /*
@@ -51,7 +52,6 @@ Token.prototype.isOperator = function() {
  */
 Token.prototype.isMethod = function() {
     return this.type === 'method';
-    return this._value.match(/^[A-Z]+$/);
 };
 
 /**
@@ -59,15 +59,6 @@ Token.prototype.isMethod = function() {
  */
 Token.prototype.isUri = function() {
     return this.type === 'uri';
-    return this._value.match(/^[a-zA-Z]+:\/\/.+/);
 };
-/*
-Token.prototype.value = function() {
-    if (this.isOperator()) {
-        return operators[this._value];
-    } else if (this.isUri() || this.isMethod()) {
-        return this._value;
-    }
-};
-*/
+
 module.exports = Token;
