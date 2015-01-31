f5fbe251fa0f1b59a57677f78ba3f7d6f82ea0f2
Tim Rodger <tim.rodger@gmail.com>
2015-01-31 11:00:14 +0000

    Adds more tests of server

--- src/tests/server_test.js
+++ src/tests/server_test.js
@@ -5,51 +5,70 @@ var server = require('../server'),
     request = require('supertest'),
     nock = require('nock');
 
-describe('GET /', function() {
+describe('server', function() {
 
     before(function(done) {
         server.start();
         done();
     });
 
-    it('responds with json', function(done) {
-        request(server.app)
-            .get('/')
-            .set('Accept', 'text/plain')
-            .expect('Content-Type', /json/)
-            .expect(200, done);
-    });
-});
+    describe('GET /', function() {
 
-describe('POST /', function() {
-    
-    before(function(done) {
-        server.start();
-        done();
+        it('responds with json', function(done) {
+            request(server.app)
+                .get('/')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
     });
-    
-    it('responds with success for empty script', function(done) {
-        request(server.app)
-            .post('/')
-            .set('Content-Type', 'text/plain')
-            .send('')
-            .expect(200, done);
-    });
-    
-    it('responds with success for single command script', function(done) {
-        // mock the thing endpoint
-        var endpoint = 'http://testing';
 
-        var mock = nock(endpoint)
-            .get('/1')
-            .reply(200, {
-                data: "text"
-            });
+    describe('POST /', function() {
+    
+        it('responds with success for empty script', function(done) {
+            request(server.app)
+                .post('/')
+                .set('Content-Type', 'text/plain')
+                .send('')
+                .expect(200, done);
+        });
+    
+        it('responds with success for single command script', function(done) {
+            var endpoint = 'http://testing';
 
-        request(server.app)
-            .post('/')
-            .set('Content-Type', 'text/plain')
-            .send('GET ' + endpoint + '/1')
-            .expect(200, done);
+            var mock = nock(endpoint)
+                .get('/1')
+                .reply(200, {
+                    data: "text"
+                });
+
+            request(server.app)
+                .post('/')
+                .set('Content-Type', 'text/plain')
+                .send('GET ' + endpoint + '/1')
+                .expect(200, done);
+        });
+    
+        it('pipes one response into the next request', function(done) {
+            var first_response = {data: 'text'};
+            var first = 'http://testing';
+
+            var mock_first = nock(first)
+                .get('/1', '')
+                .reply(200, first_response);
+
+            var second = 'http://second';
+
+            var mock_second = nock(second)
+                .post('/', first_response)
+                .reply(200, {
+                    data: "text"
+                });
+
+            request(server.app)
+                .post('/')
+                .set('Content-Type', 'text/plain')
+                .send('GET ' + first + '/1' + ' POST ' + second + '/')
+                .expect(200, done);
+        });
     });
 });
