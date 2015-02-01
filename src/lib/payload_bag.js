3a69287beab767801cc63777231fdb8dae0f61de
Tim Rodger <tim.rodger@gmail.com>
2015-02-01 14:56:34 +0000

    Enforces private vars in PayloadBag

--- src/lib/payload_bag.js
+++ src/lib/payload_bag.js
@@ -2,66 +2,68 @@ var Payload = require('./payload'),
     _ = require('underscore');
 
 /**
- * Container for incoming responses 
+ * Container for incoming response Payloads
  */
 function PayloadBag(size) {
-    this.size = size;
-    this.contents = [];
-}
+    // declare private vars
+    var size = size;
+    var contents = [];
+    var that = this;
 
-PayloadBag.prototype.total = function() {
-    return this.size;
-};
+    this.total = function() {
+        return size;
+    };
 
-PayloadBag.prototype.currentSize = function() {
-    return this.contents.length;
-};
+    this.currentSize = function() {
+        return contents.length;
+    };
 
-PayloadBag.prototype.full = function() {
-    return this.contents.length == this.size;
-};
+    this.full = function() {
+        return contents.length == size;
+    };
 
-PayloadBag.prototype.push = function(e, p) {
-    this.contents.push(
-        {
-            error: e,
-            payload: p
-        }
-    );
-};
+    this.push = function(e, p) {
+        contents.push(
+            {
+                error: e,
+                payload: p
+            }
+        );
+    };
 
-PayloadBag.prototype.errors = function() {
-    for (var res in this.contents) {
-        if (this.contents[res].error) {
-            return true;
-        }
-    }
-    return false;
-};
-
-/**
- * If there is 2 or more responses return a Payload containing an array of the responses
- * but join responses that are arrays into a single array
- * If there is 1 response return that Payload
- * If there are 0 responses return a Payload with empty contents
- */
-PayloadBag.prototype.join = function() {
-    if (this.contents.length == 0) {
-        return new Payload('');
-    } else if (this.contents.length == 1) {
-        return this.contents[0].payload;
-    } else {
-        var all = [], value;
-        for (var i in this.contents) {
-            value = this.contents[i].payload.value;
-            if (_.isArray(value)) {
-                all = all.concat(value);
-            } else {
-                all.push(this.contents[i].payload.content);
+    this.errors = function() {
+        for (var res in contents) {
+            if (contents[res].error) {
+                return true;
             }
         }
-        return new Payload(all);
-    }
-};
+        return false;
+    };
+
+    /**
+    * If there is 2 or more responses return a Payload containing an array of the responses
+    * but join responses that are arrays into a single array
+    * If there is 1 response return that Payload
+    * If there are 0 responses return a Payload with empty contents
+    */
+    PayloadBag.prototype.join = function() {
+        if (that.currentSize() == 0) {
+            return new Payload('');
+        } else if (that.currentSize() == 1) {
+            return contents[0].payload;
+        } else {
+            var all = [], value;
+            for (var i in contents) {
+                value = contents[i].payload.value;
+                if (_.isArray(value)) {
+                    all = all.concat(value);
+                } else {
+                    all.push(contents[i].payload.content);
+                }
+            }
+            return new Payload(all);
+        }
+    };
+}
 
 module.exports = PayloadBag;
