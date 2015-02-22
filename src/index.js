52946bef962e6b86df45b7acba54a272426e8da7
Tim Rodger <tim.rodger@gmail.com>
2015-02-22 12:00:35 +0000

    line formatting tidy up

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ var logger = require('./lib/logger'),
 
 // use env.PORT if set
 var PORT = process.env.PORT || 80;
+
 server.start();
 server.app.listen(PORT);
+
 logger.log('info', 'Running glue service on http://localhost:' + PORT);
