a9dd3a5408b7fdf39f1f2fef69630c66f76b898a
Tim Rodger <tim.rodger@gmail.com>
2015-02-24 21:11:34 +0000

    See if using the ip address will work when running travis tests

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
  */
 var Store = require('./lib/store');
 
-var mongo_host = process.env.MONGO_HOST || 'localhost:27017';
+var mongo_host = process.env.MONGO_HOST || '127.0.0.1:27017';
 var mongo_db = process.env.MONGO_DB || 'glue';
 var mongo_collection = 'executions';
 
