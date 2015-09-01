75237c3e9973c46834a302860bb1d7ddbc332c5c
Tim Rodger <tim.rodger@gmail.com>
2015-09-01 21:20:18 +0100

    Updates access to mongo to fall back to a default dsn

--- src/config.js
+++ src/config.js
@@ -3,8 +3,16 @@
  */
 var Store = require('./lib/store');
 
-var mongo_host = process.env.MONGO_HOST || process.env.MONGODB_PORT_27017_TCP_ADDR + ':' + process.env.MONGODB_PORT_27017_TCP_PORT;
+var mongo_host = '127.0.0.1:27017';
+
+if (process.env.MONGODB_PORT_27017_TCP_ADDR && process.env.MONGODB_PORT_27017_TCP_PORT) {
+    mongo_host = process.env.MONGODB_PORT_27017_TCP_ADDR + ':' + process.env.MONGODB_PORT_27017_TCP_PORT;
+} else if (process.env.MONGO_HOST) {
+    mongo_host = process.env.MONGO_HOST;
+}
+
 var mongo_db = process.env.MONGO_DB || 'glue';
+
 var mongo_collection = 'executions';
 
 var mongo_url = 'mongodb://' + mongo_host +'/' + mongo_db;
